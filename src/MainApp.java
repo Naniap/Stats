d1aa386418a6c55439434b471df1082c741a66a2
Mike <mikey@phantomelite.com>
2017-05-11 20:41:02 -0400

    Added julia's stuff

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,7 +1,11 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Image;
 
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -9,7 +13,19 @@ import java.awt.Canvas;
 import javax.swing.JLabel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
+import java.io.File;
+import java.io.IOException;
+class ImagePanel extends JComponent {
+    private Image image;
+    public ImagePanel(Image image) {
+        this.image = image;
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
+    }
+}
 public class MainApp extends JFrame {
 
 	private JPanel contentPane;
@@ -38,19 +54,21 @@ public class MainApp extends JFrame {
 		setTitle("Card and Dice Roll Simulator");
 		frame = this;
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 684, 412);
+		setBounds(100, 100, 360, 360);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		ImageIcon image = new ImageIcon("./resources/cards.png");
+		ImageIcon image = new ImageIcon("./resources/probability.png");
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 290, 346);
+		panel.setBounds(24, 275, 137, 36);
 		contentPane.add(panel);
 		panel.setLayout(null);
-		JLabel lblNewLabel = new JLabel("New label");
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setBounds(0, 0, 137, 36);
+		panel.add(lblNewLabel);
 		lblNewLabel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
@@ -58,17 +76,15 @@ public class MainApp extends JFrame {
 				frame.dispose();
 			}
 		});
-		lblNewLabel.setBounds(0, 0, 290, 346);
-		panel.add(lblNewLabel);
 		lblNewLabel.setIcon(image);
 		lblNewLabel.setIcon(image);
 		
 		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(310, 11, 348, 346);
+		panel_1.setBounds(171, 275, 137, 36);
 		contentPane.add(panel_1);
 		panel_1.setLayout(null);
-		ImageIcon image2 = new ImageIcon("./resources/dice.png");
-		JLabel lblNewLabel_1 = new JLabel("New label");
+		ImageIcon image2 = new ImageIcon("./resources/histogram.png");
+		JLabel lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -76,8 +92,16 @@ public class MainApp extends JFrame {
 				frame.dispose();
 			}
 		});
-		lblNewLabel_1.setBounds(0, 0, 348, 346);
+		lblNewLabel_1.setBounds(0, 0, 137, 36);
 		lblNewLabel_1.setIcon(image2);
 		panel_1.add(lblNewLabel_1);
+		ImageIcon image3 = new ImageIcon("./resources/homescreen.png");
+		JPanel panel_2 = new JPanel();
+		panel_2.setBounds(0, 0, 360, 360);
+		contentPane.add(panel_2);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(image3);
+		panel_2.add(lblNewLabel_2);
 	}
 }
